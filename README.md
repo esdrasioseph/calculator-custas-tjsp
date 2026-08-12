ad91580ad2241006a9f7d43f9ca5b84ec6b6400b
Esdras Ioseph <esdrasioseph@gmail.com>
2026-08-12 19:52:10 -0300

    feat: primeira versão da calculadora de custas

---
+++ README.md
@@ -0,0 +1,33 @@
+# Calculadora de Custas Judiciais - TJSP
+
+Projeto desenvolvido com o objetivo de auxiliar advogados, estagiários e profissionais do direito para facilitar o cálculo da taxa judiciária necessária para o ingresso de petições iniciais junto ao Tribunal de Justiça do Estado de São Paulo (TJSP).
+
+## Sobre o Projeto 
+
+O algoritmo calcula o valor da taxa judiciária devida no momento do protocolo de petições iniciais, reconvenções e embargos à execução.
+
+O cálculo é realizado com base no percentual de 1,5% a incidir sobre o valor da causa, observando-se as disposições constantes do art. 4º, I e § 1º da Lei nº 11.608 de 29 de dezembro de 2003.
+
+## Funcionalidades 
+
+**Tratamento Flexível de Entradas:** Aceita valores formatados de diferentes formas, seja com ausência de pontuação, seja com utilização de ponto, vírgula ou até mesmo o símbolo 'R$'.
+
+**Validação de Entrada e Orientação ao Usuário:** O sistema identifica valores numéricos inválidos, iguais ou menores que zero e orienta o usuário.
+
+**Interface Personalizada:** Estilização customizada com CSS.
+
+**Tratamento de Saída:** Utilização da biblioteca Babel para formatação da moeda no padrão brasileiro.
+
+## Tecnologias Utilizadas
+
+**Python:** É a linguagem básica do projeto.
+
+**Streamlit:** Framework para construção da interface web.
+
+**Babel:** Biblioteca para formatação dos valores monetários no padrão brasileiro.
+
+**CSS3:** Linguagem de estilização utilizada para customização visual dos elementos.
+
+
+
+
---
+++ app-web.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import time
+from babel.numbers import format_currency
+
+with open('./style.css') as s:
+    st.markdown(f'<style>{s.read()}</style>', unsafe_allow_html= True)
+
+st.set_page_config(page_title='Calculadora de Taxa Judiciária - TJSP', page_icon='⚖️')
+
+st.title('Calculadora de Taxa Judiciária - TJSP')
+st.text('Insira o valor da causa para calcular as custas iniciais devidas ao Tribunal de Justiça de São Paulo.')
+st.text('Esse sistema deve ser utilizado apenas para o caso de protocolo de petições iniciais, '
+'reconvenções e embargos (taxa de 1,5% conforme Lei Estadual nº 11.608/2003 do TJSP, respeitando os '
+'limites de piso e teto.')
+st.text('Esta calculadora é uma ferramenta auxiliar e não substitui a conferência das normas oficiais do TJSP. '
+'O desenvolvedor não se responsabiliza por divergências ou recolhimentos indevidos.')
+
+input_text = st.text_input('Valor da Causa (R$): ', placeholder='Ex.: 50.000,00 ou 50000')
+
+if st.button('Calcular Custas', type='primary') or input_text:
+    try:
+
+        n1 = float(input_text.replace('.', '')
+            .replace(',', '.')
+            .replace('R$', '')
+            .replace('reais', '')
+            .strip())
+
+        valor = n1 * 0.015
+
+        if valor <= 192.10:
+            resultado_texto = "R$ 192,10 (Piso Mínimo)"
+            if valor <= 0:
+                resultado_texto = "valor númerico inválido. Por favor, digite um número positivo maior que 0"
+        elif valor > 115260:
+            resultado_texto = "R$ 115.260,00 (Teto Máximo)"
+        else:
+            resultado_texto = format_currency(valor,'BRL', locale='pt_BR')
+
+        with st.spinner('Calculando...'):
+            time.sleep(1)
+
+        st.success(f'O valor das custas iniciais é de {resultado_texto}.')
+
+    except ValueError:
+        st.error('Por favor, digite um valor númerico válido (Ex.: 50.000,00 ou 50000)')
+
+st.markdown("""
+<div class="footer">
+    Desenvolvido por <a href="https://github.com/esdrasioseph" tagert="_blank">@esdrasioseph</a>
+</div>
+"""
+,unsafe_allow_html= True)
